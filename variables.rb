bcf6464503c613c89393f09ee73ed30c56244737
BMWire <bmwemawire@gmail.com>
2022-05-27 16:21:05 +0300

    chore: variables

---
+++ variables.rb
@@ -0,0 +1,29 @@
+#!/usr/bin/ruby
+
+=begin
+    * Variable initialization
+    * Variable printing
+=end
+
+name = 'Barry'
+age = 20
+country = 'Kenya'
+
+puts "Name: #{name} \nAge: #{age} \nCountry: #{country}\n"
+
+=begin
+    * Number variables
+    * String variables
+=end
+
+signed_int = -644
+unsigned_int = 44
+simple_float = 44.456
+another_float = -456.32
+binary = 0b10111
+octal = 012331
+hexadecimal = 0xff
+scientific_notation = 6.8E09
+
+puts "Signed int: #{signed_int} \nUnsigned int: #{unsigned_int} \nSimple float: #{simple_float} \nAnother float: #{another_float} \nBinary: #{binary} \nOctal: #{octal} \nHexadecimal: #{hexadecimal} \nScientific Notation: #{scientific_notation} \n"
+
