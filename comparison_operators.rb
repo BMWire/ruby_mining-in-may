03ef15c16e0642d00d3e9710a2877c27a7a433fd
BMWire <bmwemawire@gmail.com>
2022-06-08 10:40:47 +0300

    chore: create comparison operators

---
+++ comparison_operators.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/ruby
+
+# Initialize some variables
+x, y = 50, 10
+
+# Print x and y
+puts "\nx = #{x} and y = #{y}"
+
+# Print comparison results
+puts "\nx>y is #{x>y}"
+puts "\nx<y is #{x<y}"
+puts "\nx==y is #{x==y}"
+puts "\nx!=y is #{x!=y}"
+puts "\nx>=y is #{x>=y}"
+puts "\nx<=y is #{x<=y}"
+puts "\nx<=>y is #{x<=>y}"
