7513d86a6df506f5ee03718e96e8dcca2462db43
BMWire <bmwemawire@gmail.com>
2022-06-11 18:35:23 +0300

    chore: learn while, until loops

---
+++ 5_ControlStructures/until-loop.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+# Print the multiples of 3 from 3 to 90
+
+count = 1
+
+until (count == 11) do
+    puts "\n 3 * #{count} = #{count * 3}"
+
+    count += 1
+end
---
+++ 5_ControlStructures/while-loop.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+
+# A simple while loop to investigate the effect of having the increment statement
+# at different areas of the loop
+
+count = 0
+
+while (count < 5) do
+    count += 1
+    puts "Into the thick of it!"
+end
