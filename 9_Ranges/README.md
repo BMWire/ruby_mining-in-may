2e0104df2aa3cc7b8eeb6145176204a88fcb2ee4
Barry Wire <bmwemawire@gmail.com>
2022-06-29 21:37:19 +0300

    chore: create range

---
+++ 9_Ranges/README.md
@@ -0,0 +1,3 @@
+# Ranges
+
+A range is a data set having a collection of items in a sequence. The items in the range are called the elements of the range.
---
+++ 9_Ranges/range-init.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+# Numeric range
+otten = (1..10)
+
+# Range conversion to array
+otten_array = otten.to_a()
+
+# Printing
+puts "The range containing numbers from one to ten is: #{otten}. \nThe corresponding array is #{otten_array}"
+puts "The length of the array is #{otten_array.length}"
+
+
+# Alphabetical range
+iqiu = ('i'.. 'q')
+
+# Range conversion to array
+iqiu_array = iqiu.to_a()
+
+# Printing
+puts "The range containing letters from 'i' to 'q' is: #{iqiu}. \nThe corresponding array is #{iqiu_array}"
+puts "The length of the array is #{iqiu_array.length}"
