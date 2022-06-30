c3d11ff584ae5dda454a8b1b3f2ccd41c7834aea
Barry Wire <bmwemawire@gmail.com>
2022-06-30 08:03:13 +0300

    chore: finish ruby introduction

--- 9_Ranges/range-init.rb
+++ 9_Ranges/range-init.rb
@@ -20,3 +20,12 @@ iqiu_array = iqiu.to_a()
 # Printing
 puts "The range containing letters from 'i' to 'q' is: #{iqiu}. \nThe corresponding array is #{iqiu_array}"
 puts "The length of the array is #{iqiu_array.length}"
+
+# Using a for loop to print the range
+
+negthtth = (-3..3)
+
+for value in negthtth
+    puts value
+end
+
